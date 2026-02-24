b907daef351b3fe2170011758e1a7845a255fb66
K SHIVARAM <kakeshivaram@gmail.com>
2026-02-24 23:25:26 +0530

    added encapsulation file

---
+++ exception.py
@@ -0,0 +1,6 @@
+print("our code starts here ...")
+
+print("we are going to take two numbers as input from user")
+print("we are going to add two numbers")
+
+print("we are going to divide two numbers")
